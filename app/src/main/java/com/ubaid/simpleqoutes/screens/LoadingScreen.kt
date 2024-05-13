bcdd9a9695c720e1f5566e2f24899377c5e11ef3
UbaidNiazi <53142405+Ubaid92@users.noreply.github.com>
2024-05-13 20:07:43 +0500

    Some fixes

--- app/src/main/java/com/ubaid/simpleqoutes/screens/LoadingScreen.kt
+++ app/src/main/java/com/ubaid/simpleqoutes/screens/LoadingScreen.kt
@@ -2,23 +2,34 @@ package com.ubaid.simpleqoutes.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-
+@Preview
 @Composable
- fun LoadingScreen() {
+fun LoadingScreen() {
     Box(
-        modifier = Modifier.fillMaxSize(1f), contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize(1f),
     ) {
         Text(
-            text = "Loading Quotes.....",
-            style = MaterialTheme.typography.headlineMedium
-        )
-    }
+            modifier = Modifier.align(Alignment.Center),
+            text = "Loading Quotes...",
+            style = MaterialTheme.typography.headlineMedium,
 
+        )
+        Text(
+            modifier = Modifier.align(Alignment.BottomCenter)
+                .padding(0.dp,0.dp,0.dp,20.dp),
+            text = "Created by Ubaid Niazi",
+            style = MaterialTheme.typography.bodyLarge,
+
+            )
+    }
 }
+
--- app/src/main/java/com/ubaid/simpleqoutes/screens/QuoteListScreen.kt
+++ app/src/main/java/com/ubaid/simpleqoutes/screens/QuoteListScreen.kt
@@ -1,5 +1,7 @@
 package com.ubaid.simpleqoutes.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -7,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -15,18 +18,18 @@ import com.ubaid.simpleqoutes.R
 import com.ubaid.simpleqoutes.models.QuotesData
 
 
-    @Composable
-    fun QuotesListScreen(data: Array<QuotesData>, onClick: (quote:QuotesData) -> Unit) {
-        Column {
-            Text(
-                text = "Quotes App",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(8.dp, 24.dp)
-                    .fillMaxWidth(1f),
-                style = MaterialTheme.typography.headlineMedium,
-                fontFamily = FontFamily(Font(R.font.montserrat))
-            )
-            QuoteList(data = data, onClick)
-        }
+@Composable
+fun QuotesListScreen(data: Array<QuotesData>, onClick: (quote: QuotesData) -> Unit) {
+    Column {
+        Text(
+            text = "Quotes App",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(8.dp, 24.dp)
+                .fillMaxWidth(1f),
+            style = MaterialTheme.typography.headlineMedium,
+            fontFamily = FontFamily(Font(R.font.montserrat))
+        )
+        QuoteList(data = data, onClick)
     }
+}
